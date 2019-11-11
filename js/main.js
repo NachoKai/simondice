11463896a7db10a35e36b99c4ed61985511234a1
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-11-10 22:43:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,23 @@
 
 /*
 *START para correr el programa
-    >>mensaje muestra: suerte! (timer)
-    >> mensaje muestra: es el turno de la computadora 
+    >> Mensaje muestra: Suerte! >> (timer)
+    >> Mensaje muestra: Es el turno de la computadora...
     >> una funcion elige un random entre 1 y 4 (asignarle un numero a cada color?) y elige uno de los colores
-    >> se ilumina el color con un cambio de clase (timer)
-    >> mensaje muestra: es tu turno
+    >> se ilumina el color con un cambio de clase >> (timer)
+    >> Mensaje muestra: Es tu turno...
         >> si se presiona el mismo color: 
-            >> mensaje: Correcto! haz click en continuar
+            >> Mensaje: Correcto! Haz click en CONTINUAR
             >> Puntaje aumenta +1
-            >> Puntaje maximo aumenta +1 si el numero es menor a Puntaje
-            >>al clickear continuar se activa la funcion random pero ahora elije dos numeros (colores)
-            >>se ilumina un color con un cambio de clase (timer), se ilumina el otro
+            >> Puntaje Maximo aumenta +1 si el numero es menor a Puntaje
+            >> al clickear CONTINUAR se activa la funcion random pero ahora elije dos numeros (colores)
+            >> se ilumina un color con un cambio de clase (timer), se ilumina el otro
         >> si se presiona otro color 
-            >> mensaje: Incorrecto! vuelve a empezar
+            >> mensaje: Incorrecto! Vuelve a empezar
             >> se resetea Puntaje
-            >> Puntaje Maximo guarda el numero de Puntaje
+            >> Puntaje Maximo guarda el numero mas alto de Puntaje, y si es mas alto le cambia el valor y lo guarda
 
-*Puntaje que va aumentando con cada respuesta correcta, se reinicia cuando hay respuesta incorrecta
-*Puntaje Maximo guarda el numero maximo alcanzado en Puntaje.
- *Reiniciar borra el Puntaje y comienza a correr el programa de nuevo, no borra Puntaje Maximo.
-*Turno de: 
-*Continuar corre el turno siguiente si la respuesta es correcta.
- *Mensaje muestra si es turno de la Computadora o el Usuario, si la respuesta es correcta (para poder ejecutar continuar) o si es incorrecta (para ejecutar reiniciar)
+*REINICIAR borra el Puntaje y comienza a correr el programa de nuevo, no borra Puntaje Maximo.
 */
 
 const $form = document.querySelector("#form")
