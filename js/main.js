03d8beb50b4bdbd636fa93adf6285bae58e6fc67
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-11-10 21:03:10 -0300

    boceto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+// *START para correr el programa
+// *Puntaje que va aumentando con cada respuesta correcta, se reinicia cuando hay respuesta incorrecta
+// *Puntaje Maximo guarda el numero maximo alcanzado en Puntaje.
+// *Reiniciar borra el Puntaje y comienza a correr el programa de nuevo, no borra Puntaje Maximo.
+// *Turno de: 
+// *Continuar corre el turno siguiente si la respuesta es correcta.
+// *Mensaje muestra si es turno de la Computadora o el Usuario, si la respuesta es correcta (para poder ejecutar continuar) o si es incorrecta (para ejecutar reiniciar)
