101c5f8c004b87d3ffc518edec62e10d6bb2cb13
Nacho Caiafa <nachokai@outlook.com>
2020-02-09 14:46:08 -0300

    add eslint

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-const $form = document.querySelector("#form");
-let secuenciaPc = [],
-  secuenciaUser = [],
-  turnoUser = false,
-  turnos = 0,
-  ronda = 0,
-  sonidoUno = document.querySelector("#audio-uno"),
-  sonidoDos = document.querySelector("#audio-dos"),
-  sonidoTres = document.querySelector("#audio-tres"),
-  sonidoCuatro = document.querySelector("#audio-cuatro"),
-  sonidoStart = document.querySelector("#audio-start"),
-  sonidoCorrect = document.querySelector("#audio-correct"),
-  sonidoWrong = document.querySelector("#audio-wrong");
-const $mensaje = document.querySelector("#mensaje");
+const $form = document.querySelector('#form');
+let secuenciaPc = [];
+let turnoUser = false;
+let turnos = 0;
+
+const sonidoUno = document.querySelector('#audio-uno');
+const sonidoDos = document.querySelector('#audio-dos');
+const sonidoTres = document.querySelector('#audio-tres');
+const sonidoCuatro = document.querySelector('#audio-cuatro');
+const sonidoStart = document.querySelector('#audio-start');
+const sonidoCorrect = document.querySelector('#audio-correct');
+const sonidoWrong = document.querySelector('#audio-wrong');
+const $mensaje = document.querySelector('#mensaje');
 
 const SOUNDS = {
   play1: () => sonidoUno.play(),
@@ -20,89 +19,90 @@ const SOUNDS = {
   play4: () => sonidoCuatro.play(),
   playStart: () => sonidoStart.play(),
   playCorrect: () => sonidoCorrect.play(),
-  playWrong: () => sonidoWrong.play()
+  playWrong: () => sonidoWrong.play(),
 };
 
-actualizarMensaje = mensaje => ($mensaje.value = mensaje);
+function actualizarMensaje(mensaje) {
+  ($mensaje.value = mensaje);
+}
 
 function permiteClickear() {
   if (turnoUser === false) {
-    return (turnoUser = true);
+    turnoUser = true;
   }
 }
 
 function noPermiteClickear() {
   if (turnoUser === true) {
-    return (turnoUser = false);
+    turnoUser = false;
   }
 }
 
-let actualizarNumeroRonda = ronda =>
-  (document.querySelector("#ronda").textContent = ronda);
+const actualizarNumeroRonda = (ronda) => (document.querySelector('#ronda').textContent = ronda);
 
 function turnoPc() {
   noPermiteClickear();
   setTimeout(() => {
     ronda++;
     actualizarNumeroRonda(ronda);
-    actualizarMensaje("Es el turno de la computadora...");
+    actualizarMensaje('Es el turno de la computadora...');
   }, 900);
   setTimeout(() => eligeRandom(), 900);
 }
 
 function eligeRandom() {
   noPermiteClickear();
-  let nodoRojo = $form.querySelector("#rojo"),
-    nodoVerde = $form.querySelector("#verde"),
-    nodoAzul = $form.querySelector("#azul"),
-    nodoAmarillo = $form.querySelector("#amarillo"),
-    eligeRojo = 1,
-    eligeVerde = 2,
-    eligeAzul = 3,
-    eligeAmarillo = 4,
-    numerosRandoms = iteracionDeRandoms();
+  const nodoRojo = $form.querySelector('#rojo');
+  const nodoVerde = $form.querySelector('#verde');
+  const nodoAzul = $form.querySelector('#azul');
+  const nodoAmarillo = $form.querySelector('#amarillo');
+  const eligeRojo = 1;
+  const eligeVerde = 2;
+  const eligeAzul = 3;
+  const eligeAmarillo = 4;
+  const numerosRandoms = iteracionDeRandoms();
 
   for (let i = 0; i < numerosRandoms.length; i++) {
-    let tiempo = (i + 1) * 900;
+    const tiempo = (i + 1) * 900;
     const RETRASO_TURNO_JUGADOR = (numerosRandoms.length + 1) * 800;
 
     if (numerosRandoms[i] === eligeRojo) {
       setTimeout(() => {
         SOUNDS.play1();
-        nodoRojo.className = "rojoActivado";
+        nodoRojo.className = 'rojoActivado';
       }, tiempo);
       setTimeout(() => {
-        nodoRojo.className = "boton-rojo";
+        nodoRojo.className = 'boton-rojo';
       }, tiempo + 500);
     }
 
     if (numerosRandoms[i] === eligeVerde) {
       setTimeout(() => {
         SOUNDS.play2();
-        nodoVerde.className = "verdeActivado";
+        nodoVerde.className = 'verdeActivado';
       }, tiempo);
       setTimeout(() => {
-        nodoVerde.className = "boton-verde";
+        nodoVerde.className = 'boton-verde';
       }, tiempo + 500);
     }
 
     if (numerosRandoms[i] === eligeAzul) {
       setTimeout(() => {
         SOUNDS.play3();
-        nodoAzul.className = "azulActivado";
+        nodoAzul.className = 'azulActivado';
       }, tiempo);
       setTimeout(() => {
-        nodoAzul.className = "boton-azul";
+        nodoAzul.className = 'boton-azul';
       }, tiempo + 500);
     }
 
     if (numerosRandoms[i] === eligeAmarillo) {
       setTimeout(() => {
         SOUNDS.play4();
-        nodoAmarillo.className = "amarilloActivado";
+        nodoAmarillo.className = 'amarilloActivado';
       }, tiempo);
       setTimeout(() => {
-        nodoAmarillo.className = "boton-amarillo";
+        nodoAmarillo.className = 'boton-amarillo';
       }, tiempo + 500);
     }
 
@@ -115,7 +115,7 @@ function eligeRandom() {
 
 function turnoUsuario() {
   permiteClickear();
-  actualizarMensaje("Es tu turno!");
+  actualizarMensaje('Es tu turno!');
 }
 
 function iteracionDeRandoms() {
@@ -126,53 +126,53 @@ function iteracionDeRandoms() {
 }
 
 function asignarPuntajeMaximo() {
-  let nodoPMaximo = $form.pmaximo,
-    nodoPuntaje = $form.puntaje,
-    puntaje = Number(nodoPuntaje.value),
-    pmaximo = Number(nodoPMaximo.value);
+  const nodoPMaximo = $form.pmaximo;
+  const nodoPuntaje = $form.puntaje;
+  const puntaje = Number(nodoPuntaje.value);
+  const pmaximo = Number(nodoPMaximo.value);
   if (puntaje > pmaximo) {
     nodoPMaximo.value = puntaje;
   }
 }
 
 function gameOver() {
-  let nodoRojo = $form.querySelector("#rojo"),
-    nodoVerde = $form.querySelector("#verde"),
-    nodoAzul = $form.querySelector("#azul"),
-    nodoAmarillo = $form.querySelector("#amarillo");
+  const nodoRojo = $form.querySelector('#rojo');
+  const nodoVerde = $form.querySelector('#verde');
+  const nodoAzul = $form.querySelector('#azul');
+  const nodoAmarillo = $form.querySelector('#amarillo');
 
-  nodoRojo.className = "rojoActivado";
-  nodoVerde.className = "verdeActivado";
-  nodoAzul.className = "azulActivado";
-  nodoAmarillo.className = "amarilloActivado";
+  nodoRojo.className = 'rojoActivado';
+  nodoVerde.className = 'verdeActivado';
+  nodoAzul.className = 'azulActivado';
+  nodoAmarillo.className = 'amarilloActivado';
 }
 
 function gameStart() {
-  let nodoRojo = $form.querySelector("#rojo"),
-    nodoVerde = $form.querySelector("#verde"),
-    nodoAzul = $form.querySelector("#azul"),
-    nodoAmarillo = $form.querySelector("#amarillo");
+  const nodoRojo = $form.querySelector('#rojo');
+  const nodoVerde = $form.querySelector('#verde');
+  const nodoAzul = $form.querySelector('#azul');
+  const nodoAmarillo = $form.querySelector('#amarillo');
 
-  nodoRojo.className = "boton-rojo";
-  nodoVerde.className = "boton-verde";
-  nodoAzul.className = "boton-azul";
-  nodoAmarillo.className = "boton-amarillo";
+  nodoRojo.className = 'boton-rojo';
+  nodoVerde.className = 'boton-verde';
+  nodoAzul.className = 'boton-azul';
+  nodoAmarillo.className = 'boton-amarillo';
 }
 
 function turnoJugador(num) {
   permiteClickear();
-  let nodoPuntaje = $form.puntaje,
-    puntaje = Number(nodoPuntaje.value);
+  const nodoPuntaje = $form.puntaje;
+  const puntaje = Number(nodoPuntaje.value);
 
-  if (num == secuenciaPc[turnos]) {
+  if (num === secuenciaPc[turnos]) {
     if (turnos === secuenciaPc.length - 1) {
-      actualizarMensaje("Correcto!");
+      actualizarMensaje('Correcto!');
       nodoPuntaje.value = puntaje + 1;
       SOUNDS.playCorrect();
       turnoPc();
     } else {
       turnos++;
-      return "";
+      return '';
     }
   }
 
@@ -180,11 +180,11 @@ function turnoJugador(num) {
     noPermiteClickear();
     gameOver();
     SOUNDS.playWrong();
-    actualizarMensaje("Vuelve a empezar...");
+    actualizarMensaje('Vuelve a empezar...');
     failed();
     asignarPuntajeMaximo();
     mostrarBotonStart();
-    return "";
+    return '';
   }
 }
 
@@ -192,17 +192,17 @@ $form.start.onclick = () => {
   SOUNDS.playStart();
   gameStart();
   noPermiteClickear();
-  actualizarNumeroRonda("-");
+  actualizarNumeroRonda('-');
   puntaje.value = 0;
   secuenciaPc = [];
   turnos = 0;
   ronda = 0;
-  actualizarMensaje("Suerte!");
+  actualizarMensaje('Suerte!');
   ocultarBotonStart();
   turnoPc();
 };
 
-$form.querySelector("#rojo").onclick = event => {
+$form.querySelector('#rojo').onclick = (event) => {
   SOUNDS.play1();
   if (turnoUser === true) {
     turnoJugador(1);
@@ -212,7 +212,7 @@ $form.querySelector("#rojo").onclick = event => {
   }
 };
 
-$form.querySelector("#verde").onclick = event => {
+$form.querySelector('#verde').onclick = (event) => {
   SOUNDS.play2();
   if (turnoUser === true) {
     turnoJugador(2);
@@ -222,7 +222,7 @@ $form.querySelector("#verde").onclick = event => {
   }
 };
 
-$form.querySelector("#azul").onclick = event => {
+$form.querySelector('#azul').onclick = (event) => {
   SOUNDS.play3();
   if (turnoUser === true) {
     turnoJugador(3);
@@ -232,7 +232,7 @@ $form.querySelector("#azul").onclick = event => {
   }
 };
 
-$form.querySelector("#amarillo").onclick = event => {
+$form.querySelector('#amarillo').onclick = (event) => {
   SOUNDS.play4();
   if (turnoUser === true) {
     turnoJugador(4);
@@ -244,16 +244,20 @@ $form.querySelector("#amarillo").onclick = event => {
 
 function failed() {
   Swal.fire({
-    icon: "error",
-    title: "Incorrecto!",
-    text: "Intenta nuevamente, haz click en START.",
+    icon: 'error',
+    title: 'Incorrecto!',
+    text: 'Intenta nuevamente, haz click en START.',
     allowOutsideClick: true,
     allowEscapeKey: true,
-    showConfirmButton: true
+    showConfirmButton: true,
   });
 }
 
-ocultarBotonStart = () => (start.className = "hidden");
-mostrarBotonStart = () => (start.className = "button");
+ocultarBotonStart = () => {
+  (start.className = 'hidden');
+};
+mostrarBotonStart = () => {
+  (start.className = 'button');
+};
 
-document.getElementById("current-year").innerHTML = new Date().getFullYear();
+document.getElementById('current-year').innerHTML = new Date().getFullYear();
